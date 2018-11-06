2e98324a149c30c5f2cb601ed9db6f295a1e8b7f
Tahsin Ates <>
2018-11-06 13:29:36 +0000

    adding new files to github

---
+++ config/webpack.base.config.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+    ]
+}
